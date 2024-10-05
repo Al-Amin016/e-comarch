b7c70b5deaa6a610fe2b20ee9e86a4efabf18605
Al-Amin016 <alaminislam0.dev@gmail.com>
2024-10-05 12:36:47 +0600

    new commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 
 
-const bayNowBtn = document.getElementsByClassName('bayNowBtn');
+// const bayNowBtn = document.getElementsByClassName('bayNowBtn');
 
-for(bayBtn of bayNowBtn){
-    bayBtn.addEventListener('click', function(e){
-        console.log('alain islam')
-    })
-}
+// for(bayBtn of bayNowBtn){
+//     bayBtn.addEventListener('click', function(e){
+//         console.log('alain islam')
+//     })
+// }
 
-const womanSection = document.getElementById('womanSection').addEventListener('click', function(){
-    window.location.href = './woman.html'
-})
-const manSection = document.getElementById('manSection').addEventListener('click', function(){
-    window.location.href = './man.html'
-})
+// const womanSection = document.getElementById('womanSection').addEventListener('click', function(){
+//     window.location.href = './woman.html'
+// })
+// const manSection = document.getElementById('manSection').addEventListener('click', function(){
+//     window.location.href = './man.html'
+// })
